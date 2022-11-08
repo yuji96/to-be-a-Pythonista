bf4eed1f701cbdabd01ac9d03b819238f8355659
yuji96 <y.yama683@gmail.com>
2022-11-08 20:45:43 +0900

    add 2_number

---
+++ source/_build/jupyter_execute/python/operator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1. 四則演算と変数
+
+# In[1]:
+
+
+print(12345)
+
+
+# In[2]:
+
+
+print(1 + 2)
+print(3 - 4)
+print(5 * 6)
+print(7 / 8)
+
+
+# In[ ]:
+
+
+
+
